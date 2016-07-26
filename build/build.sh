4b812456b2d31496d03175ed34742ce7f7b0831d
zhouyq <zyq916@gmail.com>
2016-07-26 16:20:53 +0800

    通过参数执行函数

--- build/build.sh
+++ build/build.sh
@@ -6,22 +6,4 @@ set -e
 
 source $BUILD_DIR/functions
 
-system_init
-
-install_packages
-
-install_gosu
-
-install_libjemalloc
-
-install_gifsicle
-
-install_pngcrush
-
-install_pngquant
-
-install_ruby
-
-install_discourse
-
-install_nginx
+$1
